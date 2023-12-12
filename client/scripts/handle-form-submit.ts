a7c6ce739490aae86045e7952f8c7dcb2c864932
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-12-12 15:57:44 -0500

    Update handle-form-submit.ts

--- client/scripts/handle-form-submit.ts
+++ client/scripts/handle-form-submit.ts
@@ -9,16 +9,14 @@ import { setLoadingProgramResponse } from "./set-loading-program-response";
 export async function handleFormSubmit({ mood }: { mood: string }) {
   setLoadingProgramResponse(true);
 
-  const body = JSON.stringify({
-    mood,
-  } as CreateProgramBody);
-
   await fetch(`${WORKER_BASE_URL}/create-program`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body,
+    body: JSON.stringify({
+      mood,
+    } as CreateProgramBody),
   })
     .then((response) => response.json())
     .then((data: CreateProgramResponse) => {
