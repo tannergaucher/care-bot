c235662c53d15f3c653bdc33890ae4371a4b4d54
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-12-13 13:17:00 -0500

    handle ts and html pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,10 @@ export default {
   build: {
     rollupOptions: {
       input: {
-        index: "./index.ts",
-        program: "./program.ts",
+        index: "./index.html",
+        program: "./program.html",
+        "index-ts": "./index.ts",
+        "program-ts": "./program.ts",
       },
     },
   },
